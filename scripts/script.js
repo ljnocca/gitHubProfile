047a4acbb04f4c90a9288217b07012c1985622fa
Laurent Nocca <ljnocca@gmail.com>
2017-02-22 20:31:24 -0600

    final draft

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,5 @@
 //*** SECTION: PROFILE ***
 
-var profilePromise = $.getJSON('https://api.github.com/users/ljnocca')
-//getJson (a method contained in jQuery) makes a request to the server at that location
-//it returns a 'promise' that will resolve when the response to the request comes back.
-
-
 function profiletoHTML (profileObj){
 //this function assigns html to the API information from GitHub	
 	var profileNode = document.querySelector('.profile')
@@ -18,26 +13,18 @@ function profiletoHTML (profileObj){
 	profileObjToHTML += 	'<li class = "email"><a href="mailto:' + profileObj.email + '">'+ profileObj.email +'</a></li>'
 	profileObjToHTML += 	'<li class = "website"><a href="' + profileObj.blog + '">' + profileObj.blog + '</a></li>'
 	profileObjToHTML += '</ul>'
-
 	profileNode.innerHTML = profileObjToHTML
 }
 
-profilePromise.then(profiletoHTML)
-//promise runs the profiletoHTML function once the information is received from the server
-
-
-
 //*** SECTION: REPOS ***
-
-var reposPromise = $.getJSON('https://api.github.com/users/ljnocca/repos')
 
 function repoToHTMLstring (singleRepoObject){
 	var objectToHTML = ''
 	objectToHTML += '<ul class = "repoList">'
 	objectToHTML +=		'<li>'
 	objectToHTML += 		'<div><h1 class = "repoName">' + singleRepoObject.name + '</h1></div>'
-	objectToHTML += 		'<div><p class = "language">' + singleRepoObject.language + '</p></div>'	
-	// objectToHTML += '<p class = "lastUpdate"' + singleRepoObject.updated_at + '</p>'  <--- will not display date
+	objectToHTML += 		'<div class = "subRepoText"><p class = "language">' + singleRepoObject.language + '</p></div>'
+	objectToHTML += 		'<div class = "subRepoText"><p class = "lastUpdate">Updated ' + singleRepoObject.updated_at + '</p></div>'
 	objectToHTML +=		'</li>'
 	objectToHTML += '</ul>'
 
@@ -50,10 +37,38 @@ function arrayToCode (arrayOfRepos){
 	for (var i = 0; i<arrayOfRepos.length; i++){
 		arrayToHTML += repoToHTMLstring(arrayOfRepos[i])
 	}
-
 	repoNode.innerHTML = arrayToHTML
 }
 
-reposPromise.then(arrayToCode)
+// *** SECTION: SEARCHBAR & PROMISES
+function loadDefaultProfile (){
+	var profilePromise = $.getJSON('https://api.github.com/users/ljnocca')
+	profilePromise.then(profiletoHTML)
+
+	var reposPromise = $.getJSON('https://api.github.com/users/ljnocca/repos')
+	reposPromise.then(arrayToCode)
+}
+
+loadDefaultProfile() //automatically display the default profile
+
+var baseURL = 'https://api.github.com/users/'
+
+function searchQuery (username){
+	var profilePromise = $.getJSON(baseURL+username)
+	profilePromise.then(profiletoHTML)
+
+	var repoPromise = $.getJSON(baseURL+username+'/repos')
+	repoPromise.then(arrayToCode)
+}
+
+
+var inputSearch = document.querySelector('.searchBar')
+
+inputSearch.addEventListener('keydown', function(eventObj) {
+    if (eventObj.keyCode === 13) {
+        searchQuery(eventObj.target.value)
+    }
+})
+
 
 
